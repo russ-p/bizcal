06c465fe76db0e21e656f4430b4738e180af7f77
frebe <frebe>
2006-01-18 15:30:47 +0000

    no message

--- src/bizcal/swing/MonthView.java
+++ src/bizcal/swing/MonthView.java
@@ -1,9 +1,13 @@
 package bizcal.swing;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.LayoutManager;
+import java.awt.Rectangle;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
@@ -19,7 +23,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
@@ -27,26 +30,29 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import javax.swing.border.EtchedBorder;
 import javax.swing.plaf.basic.BasicBorders;
 
 import bizcal.common.CalendarViewConfig;
 import bizcal.common.Event;
 import bizcal.swing.util.ErrorHandler;
+import bizcal.swing.util.FrameArea;
 import bizcal.swing.util.TableLayoutPanel;
 import bizcal.swing.util.TableLayoutPanel.Row;
 import bizcal.util.BizcalException;
+import bizcal.util.DateInterval;
 import bizcal.util.DateUtil;
 import bizcal.util.TextUtil;
+import bizcal.util.Tuple;
 
 public class MonthView
 	extends CalendarView
 {
-	private static int WEEKDAY_ROW_HEIGHT = 20;
-
 	private int width;
-	private TableLayoutPanel _panel;
-	private ColumnHeaderPanel columnHeader;	
+	private JPanel _panel;
+	private ColumnHeaderPanel columnHeader;
+	private List cells = new ArrayList();
+	private List hLines = new ArrayList();
+	private List vLines = new ArrayList();
 	
 	public MonthView(CalendarViewConfig desc)
 		throws Exception
@@ -57,25 +63,17 @@ public class MonthView
 	public void refresh0()
 		throws Exception
 	{
-		_panel.deleteRows();
-		_panel.deleteColumns();
-		_panel.clear();
-		
-		_panel.setBackground(Color.WHITE);
-
-		for (int i=0; i < 7; i++)
-			_panel.createColumn(TableLayoutPanel.FILL);
-				
+		_panel.removeAll();
+		cells.clear();
+		hLines.clear();
+		vLines.clear();
+						
 		Calendar cal = Calendar.getInstance(Locale.getDefault());
 		cal.setTime(getInterval().getStartDate());
 						
 		int month = cal.get(Calendar.MONTH);
 		
 		cal.set(Calendar.DAY_OF_MONTH, 1);		
-        /*int col = cal.get(Calendar.DAY_OF_WEEK); 
-    	col -= cal.getFirstDayOfWeek();
-    	if (col < 0)
-    		col += 7;*/
 		cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
 
     	int lastDayOfWeek = cal.getFirstDayOfWeek();
@@ -84,41 +82,58 @@ public class MonthView
     		lastDayOfWeek += 7;
     	
     	Map eventMap = createEventsPerDay();
-    	
-    	Row row = _panel.createRow(TableLayoutPanel.FILL);    	
-    	/*for (int i=0; i < col; i++) {
-    		row.createCell();
-        }*/
+    	List row = new ArrayList();
+    	cells.add(row);
         while (true) {
-        	row.createCell(createDayCell(cal, eventMap, month), TableLayoutPanel.FULL, TableLayoutPanel.FULL);
+        	JComponent cell = createDayCell(cal, eventMap, month);
+        	_panel.add(cell);
+        	row.add(cell);
             if (cal.get(Calendar.DAY_OF_WEEK) == lastDayOfWeek) {
             	if (cal.get(Calendar.MONTH) != month)
             		break;
-            	row = _panel.createRow(TableLayoutPanel.FILL);
+            	row = new ArrayList();
+            	cells.add(row);
             }
             cal.add(Calendar.DAY_OF_MONTH, 1);
         }
-                   
-        _panel.updateUI();
+
+        int colCount = getModel().getSelectedCalendars().size()*7;
+        for (int i=0; i < colCount-1; i++) {
+			JLabel line = new JLabel();
+			line.setBackground(Color.LIGHT_GRAY);
+			line.setOpaque(true);
+			_panel.add(line);     
+			vLines.add(line);
+        }
+        
+        int rowCount = cells.size()-1;
+        for (int i=0; i < rowCount; i++) {
+			JLabel line = new JLabel();
+			line.setBackground(Color.LIGHT_GRAY);
+			line.setOpaque(true);
+			_panel.add(line);
+			hLines.add(line);
+        }
         
 		columnHeader.setModel(getModel());
 		columnHeader.setPopupMenuCallback(popupMenuCallback);
-		columnHeader.refresh();
-        
+		columnHeader.refresh();       
 	}
+	
 	
 	private JComponent createDayCell(Calendar cal, Map eventMap, int month)
 		throws Exception
 	{ 	
 		Font eventFont = this.font;
 		TableLayoutPanel panel = new TableLayoutPanel();
-		if (cal.get(Calendar.MONTH) == month) 
+		if (cal.get(Calendar.MONTH) == month) { 
 			panel.setBackground(Color.WHITE);
-		else 
+			panel.setOpaque(false);
+		} else  
 			panel.setBackground(new Color(230, 230, 230));
 		panel.createColumn(TableLayoutPanel.FILL);
 		//panel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-		panel.setBorder(BasicBorders.getRadioButtonBorder());
+		//panel.setBorder(BasicBorders.getRadioButtonBorder());
 		int dayno = cal.get(Calendar.DAY_OF_MONTH);
 		String text = "" + dayno;
 		Row row = panel.createRow();
@@ -158,9 +173,8 @@ public class MonthView
 		}
 		JScrollPane scrollPanel = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPanel.setPreferredSize(new Dimension(100,100));
-		return scrollPanel;
-		//panel.setPreferredSize(new Dimension(100, 100));
-		//return panel;
+		//return scrollPanel;
+		return panel;
 	}
 
 	
@@ -309,7 +323,9 @@ public class MonthView
 	protected JComponent createCalendarPanel()
 	throws Exception
 	{
-		_panel = new TableLayoutPanel();
+		_panel = new JPanel();
+		_panel.setLayout(new Layout());
+		_panel.setBackground(Color.WHITE);
 		_panel.addComponentListener(new ThisComponentListener());
 		return _panel;
 	}
@@ -348,5 +364,71 @@ public class MonthView
 	}
 	
 
+	private class Layout implements LayoutManager {
+		public void addLayoutComponent(String name, Component comp) {
+		}
+
+		public void removeLayoutComponent(Component comp) {
+		}
+
+		public Dimension preferredLayoutSize(Container parent) {
+			try {
+				int width = 7 * getModel().getSelectedCalendars().size() * DayView.PREFERRED_DAY_WIDTH;
+				return new Dimension(width, getPreferredHeight());
+			} catch (Exception e) {
+				throw BizcalException.create(e);
+			}
+		}
+
+		public Dimension minimumLayoutSize(Container parent) {
+			return new Dimension(50, 100);
+		}
+
+		public void layoutContainer(Container parent) 
+		{
+			try {
+				double width = parent.getWidth();
+				width = width / getModel().getSelectedCalendars().size();
+				width = width / 7;
+				double height = parent.getHeight() / cells.size();
+				for (int row=0; row < cells.size(); row++) {
+					List rowList = (List) cells.get(row);
+					for (int col=0; col < rowList.size(); col++) {
+						JComponent cell = (JComponent) rowList.get(col);
+						cell.setBounds((int) (col*width),
+								(int) (row*height),
+								(int) width,
+								(int) height);
+					}
+				}
+				
+		        int colCount = getModel().getSelectedCalendars().size()*7;
+		        for (int i=0; i < colCount-1; i++) {		        	
+					JLabel line = (JLabel) vLines.get(i);
+					line.setBounds((int) ((i+1)*width), 
+							0,
+							1,
+							parent.getHeight());
+					System.err.println("MonthView: " + (colCount+1)*width + ", " + 0 + ", " + 1 + ", " + parent.getHeight());
+		        }
+		        int rowCount = cells.size()-1;
+		        for (int i=0; i < rowCount; i++) {
+					JLabel line = (JLabel) hLines.get(i);
+					line.setBounds(0, 
+							(int) ((i+1)*height),
+							parent.getWidth(),
+							1);
+		        }
+				
+			} catch (Exception e) {
+				throw BizcalException.create(e);
+			}
+		}
+	}
+	
+	private int getPreferredHeight()
+	{
+		return cells.size() * 100;		
+	}
 	
 }
