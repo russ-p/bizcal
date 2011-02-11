364ce6a7c691625e0b32dbb8cf0e99da513fc4c2
hermenj <hermenj>
2011-02-11 07:17:50 +0000

    small bugfix

--- src/bizcal/util/DateUtil.java
+++ src/bizcal/util/DateUtil.java
@@ -387,7 +387,7 @@ public class DateUtil
         cal.set(Calendar.HOUR_OF_DAY, 23);
         cal.set(Calendar.MINUTE,      59);
         cal.set(Calendar.SECOND, 	  59);
-        cal.set(Calendar.MILLISECOND, 59);
+        cal.set(Calendar.MILLISECOND, 999);
         
         return cal.getTime();
 		/* ================================================== */
